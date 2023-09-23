f1f5d14468408e611ac583911dc7aba2265453df
dimilidi <dimitrovalidia@yahoo.com>
2023-09-23 03:37:35 +0200

    sSm Dgits from Homeworks_And_Labs.L02_Java_Fundamentals_Data_Types_And_Variables_EXC solcved

--- Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Data_Types_And_Variables_EXC/IntegerOperations.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Data_Types_And_Variables_EXC/IntegerOperations.java
@@ -6,6 +6,15 @@ public class IntegerOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int numOne = Integer.parseInt(scanner.nextLine());
+        int numTwo = Integer.parseInt(scanner.nextLine());
+        int numThree = Integer.parseInt(scanner.nextLine());
+        int numFour = Integer.parseInt(scanner.nextLine());
+        int sum = numOne + numTwo;
+        int division = sum / numThree;
+        int multiplication = division * numFour;
+
+        System.out.println(multiplication);
 
     }
 }
