c1e8dab27c1fe41da1ef405aab685b72e5786fae
dimilidi <dimitrovalidia@yahoo.com>
2023-09-26 12:58:39 +0200

    ArrayRotation task solved

--- Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/ArrayRotation.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/ArrayRotation.java
@@ -6,5 +6,27 @@ public class ArrayRotation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String stringInput = scanner.nextLine();
+        String[] arr = stringInput.split(" ");
+        int rotations = Integer.parseInt(scanner.nextLine());
+        String result = "";
+
+        if(rotations > arr.length) {
+            rotations = rotations % arr.length; // effective number of rotations
+        }
+
+        String endElements = "";
+        for (int i = 0; i < rotations; i++) {
+            endElements += arr[i] + " ";
+        }
+
+        String restElements = "";
+        for (int i = rotations; i < arr.length; i++) {
+            restElements += arr[i] + " ";
+        }
+
+        result = restElements +  endElements;
+
+        System.out.println(result);
     }
 }
---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/TopIntegers.java
@@ -0,0 +1,11 @@
+package Homeworks_And_Labs.L02_Java_Fundamentals_Arrays_EXC;
+
+import java.util.Scanner;
+
+public class TopIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
