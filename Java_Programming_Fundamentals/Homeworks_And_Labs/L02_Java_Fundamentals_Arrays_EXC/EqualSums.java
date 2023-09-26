e8a5fab62e6113e56d5a954f7fa7b2fdb3d72c1c
dimilidi <dimitrovalidia@yahoo.com>
2023-09-26 13:59:27 +0200

    EqualSums task solved

--- Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/EqualSums.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/EqualSums.java
@@ -6,6 +6,34 @@ public class EqualSums {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] inputArr = scanner.nextLine().split(" ");
+        int[] numbers = new int[inputArr.length];
 
+        for (int i = 0; i < inputArr.length; i++) {
+            numbers[i] = Integer.parseInt(inputArr[i]);
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            int leftSum = 0;
+            int rightSum = 0;
+
+            // Calculate the sum of elements on the left
+            for (int j = 0; j < i; j++) {
+                leftSum += numbers[j];
+            }
+
+            // Calculate the sum of elements on the right
+            for (int j = i + 1; j < numbers.length; j++) {
+                rightSum += numbers[j];
+            }
+
+            if (leftSum == rightSum) {
+                System.out.println(i);
+                return;
+            }
+        }
+
+        System.out.println("no");
     }
+
 }
