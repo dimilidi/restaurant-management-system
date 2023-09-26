4106323b020134cab8ae7ceaac5c244a0b484d61
dimilidi <dimitrovalidia@yahoo.com>
2023-09-26 13:48:59 +0200

    TopIntegers task solved

--- Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/TopIntegers.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/TopIntegers.java
@@ -6,6 +6,25 @@ public class TopIntegers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] inputArr = scanner.nextLine().split(" ");
+        String result = "";
 
+        for (int i = 0; i < inputArr.length; i++) {
+            int currentNumber = Integer.parseInt(inputArr[i]);
+            boolean isTopInteger = true;
+
+            for (int j = i + 1; j < inputArr.length; j++) {
+                if (currentNumber <= Integer.parseInt(inputArr[j])) {
+                    isTopInteger = false;
+                    break;
+                }
+            }
+
+            if (isTopInteger) {
+                result += currentNumber + " ";
+            }
+        }
+
+        System.out.println(result);
     }
 }
