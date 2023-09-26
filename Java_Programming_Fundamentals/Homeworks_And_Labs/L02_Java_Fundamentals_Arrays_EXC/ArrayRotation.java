ec7c2b729c015f1e4feee6e19b049683e30c8b14
dimilidi <dimitrovalidia@yahoo.com>
2023-09-26 12:07:49 +0200

    ZigZagArrays task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/ArrayRotation.java
@@ -0,0 +1,10 @@
+package Homeworks_And_Labs.L02_Java_Fundamentals_Arrays_EXC;
+
+import java.util.Scanner;
+
+public class ArrayRotation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
--- Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/ZigZagArrays.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L02_Java_Fundamentals_Arrays_EXC/ZigZagArrays.java
@@ -6,5 +6,24 @@ public class ZigZagArrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int countLines = Integer.parseInt(scanner.nextLine());
+        String resultEvenLine = "";
+        String resultOddLine = "";
+
+        for (int i = 0; i < countLines; i++) {
+            String[] arr = scanner.nextLine().split(" ");
+
+            if(i % 2 == 0) {
+                resultEvenLine += arr[0] + " ";
+                resultOddLine += arr[1] + " ";
+            } else {
+                resultEvenLine += arr[1] + " ";
+                resultOddLine += arr[0] + " ";
+            }
+        }
+
+        System.out.println(resultEvenLine);
+        System.out.println(resultOddLine);
+
     }
 }
