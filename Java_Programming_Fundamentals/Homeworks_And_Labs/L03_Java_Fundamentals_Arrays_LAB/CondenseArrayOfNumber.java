2f0a6ff434fbabae3cce3b4d6476be0c032a27af
dimilidi <dimitrovalidia@yahoo.com>
2023-09-29 01:26:58 +0200

    EqualArrays task done

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L03_Java_Fundamentals_Arrays_LAB/CondenseArrayOfNumber.java
@@ -0,0 +1,11 @@
+package Homeworks_And_Labs.L03_Java_Fundamentals_Arrays_LAB;
+
+import java.util.Scanner;
+
+public class CondenseArrayOfNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
