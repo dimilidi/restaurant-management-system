ce39e155a9d9bd23d113938115d650b658bff4d0
dimilidi <dimitrovalidia@yahoo.com>
2023-09-29 18:29:52 +0200

    Methods_EXC/ PalindromeIntegers task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L04_Java_Fundamentals_Methods_EXC/TopNumber.java
@@ -0,0 +1,9 @@
+package Homeworks_And_Labs.L04_Java_Fundamentals_Methods_EXC;
+
+import java.util.Scanner;
+
+public class TopNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
