4b5a76854a43fb7497e375c18816f8f861c1618a
dimilidi <dimitrovalidia@yahoo.com>
2023-09-29 12:22:39 +0200

    Methods_EXC/ VowelsCount task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L04_Java_Fundamentals_Methods_EXC/CharactersInRange.java
@@ -0,0 +1,12 @@
+package Homeworks_And_Labs.L04_Java_Fundamentals_Methods_EXC;
+
+import java.util.Scanner;
+
+public class CharactersInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
+
