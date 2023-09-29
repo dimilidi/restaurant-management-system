ec8afaa25f1e392a776c1b0e24666e0b9e2b2955
dimilidi <dimitrovalidia@yahoo.com>
2023-09-29 16:39:54 +0200

    Methods_EXC/ AddAndSubtract task solved

--- Java_Programming_Fundamentals/Homeworks_And_Labs/L04_Java_Fundamentals_Methods_EXC/AddAndSubtract.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L04_Java_Fundamentals_Methods_EXC/AddAndSubtract.java
@@ -5,5 +5,21 @@ import java.util.Scanner;
 public class AddAndSubtract {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        int num3 = Integer.parseInt(scanner.nextLine());
+
+        int sum = sum(num1, num2);
+        System.out.println(subtract(sum, num3));
+    }
+
+
+    public static int sum(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+
+    public static int subtract(int sum, int num) {
+        return sum - num;
     }
 }
---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L04_Java_Fundamentals_Methods_EXC/MiddleCharacters.java
@@ -0,0 +1,10 @@
+package Homeworks_And_Labs.L04_Java_Fundamentals_Methods_EXC;
+
+import java.util.Scanner;
+
+public class MiddleCharacters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
