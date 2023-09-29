edb9037f166d41bd17b90479f0c4589f34eeb1de
dimilidi <dimitrovalidia@yahoo.com>
2023-09-29 14:49:25 +0200

    Methods_EXC/ PasswordValidator task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L04_Java_Fundamentals_Methods_EXC/AddAndSubtract.java
@@ -0,0 +1,9 @@
+package Homeworks_And_Labs.L04_Java_Fundamentals_Methods_EXC;
+
+import java.util.Scanner;
+
+public class AddAndSubtract {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
