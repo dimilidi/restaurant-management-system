0a82e11b2a2a5d8ca676604a5cb7f072a2627cc9
dimilidi <dimitrovalidia@yahoo.com>
2023-10-02 21:51:19 +0200

    Lists_EXC/ PokemonDontGo task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L05_Java_Fundamentals_Lists_EXC/SoftUniCoursePlanning.java
@@ -0,0 +1,9 @@
+package Homeworks_And_Labs.L05_Java_Fundamentals_Lists_EXC;
+
+import java.util.Scanner;
+
+public class SoftUniCoursePlanning {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
