be75c8f7ddc12a28b4cc55edc948afe80cb482f2
dimilidi <dimitrovalidia@yahoo.com>
2023-10-06 14:59:05 +0200

    AssociativeArraysLAB/ OddOccurrences task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L08_Java_Fundamentals_Associative_Arrays_LAB/WordFilter.java
@@ -0,0 +1,9 @@
+package Homeworks_And_Labs.L08_Java_Fundamentals_Associative_Arrays_LAB;
+
+import java.util.Scanner;
+
+public class WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
