a139c3bfb790efe9eaa39d7d2d5b3f0cfec483d5
dimilidi <dimitrovalidia@yahoo.com>
2023-10-06 14:02:57 +0200

    AssociativeArraysLAB/ WordSynonyms task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L08_Java_Fundamentals_Associative_Arrays_LAB/OddOccurrences.java
@@ -0,0 +1,10 @@
+package Homeworks_And_Labs.L08_Java_Fundamentals_Associative_Arrays_LAB;
+
+import java.util.Scanner;
+
+public class OddOccurrences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
