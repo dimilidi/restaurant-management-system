4b142604a821e0773a2cc822c99e98a5af47fe02
dimilidi <dimitrovalidia@yahoo.com>
2023-10-11 23:35:07 +0200

    AssociativeArraysEXC / SoftUniParking task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L08_Java_Fundamentals_Associative_Arrays_EXC/Courses.java
@@ -0,0 +1,11 @@
+package Homeworks_And_Labs.L08_Java_Fundamentals_Associative_Arrays_EXC;
+
+import java.util.Scanner;
+
+public class Courses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
