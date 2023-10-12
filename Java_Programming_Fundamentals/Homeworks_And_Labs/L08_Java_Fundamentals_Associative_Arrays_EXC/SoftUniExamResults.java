8877d36b7c3f17bc5edc142457a642bd52127f8f
dimilidi <dimitrovalidia@yahoo.com>
2023-10-12 21:51:25 +0200

    AssociativeArraysEXC / ForceBook task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L08_Java_Fundamentals_Associative_Arrays_EXC/SoftUniExamResults.java
@@ -0,0 +1,9 @@
+package Homeworks_And_Labs.L08_Java_Fundamentals_Associative_Arrays_EXC;
+
+import java.util.Scanner;
+
+public class SoftUniExamResults {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
