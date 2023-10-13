9594cbc79b1ebde0c7b3b73a715923521a496eb1
dimilidi <dimitrovalidia@yahoo.com>
2023-10-14 00:46:48 +0200

    TextProcessingEXC / ExtractFile task solved

--- Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/CaesarCipher.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/CaesarCipher.java
@@ -6,5 +6,13 @@ public class CaesarCipher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String encrypted = "";
+        String text = scanner.nextLine();
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = (char) (text.charAt(i) + 3);
+            encrypted += c;
+        }
+        System.out.println(encrypted);
     }
 }
--- Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/MultiplyBigNumber.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/MultiplyBigNumber.java
@@ -1,2 +1,9 @@
-package Homeworks_And_Labs.L09_Java_Fundamentals_Text_Processing_EXC;public class MultiplyBigNumber {
+package Homeworks_And_Labs.L09_Java_Fundamentals_Text_Processing_EXC;
+
+import java.util.Scanner;
+
+public class MultiplyBigNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
