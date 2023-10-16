924f9f0fc40a7968c777a9d334b39de0b9fb8324
dimilidi <dimitrovalidia@yahoo.com>
2023-10-16 02:23:48 +0200

    Java_Fundamentals_TextProcessing_EXC / StringExplosion task solved

---
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/LettersChangeNumbers.java
@@ -0,0 +1,9 @@
+package Homeworks_And_Labs.L09_Java_Fundamentals_Text_Processing_EXC;
+
+import java.util.Scanner;
+
+public class LettersChangeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
--- Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/StringExplosion.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/StringExplosion.java
@@ -6,6 +6,23 @@ public class StringExplosion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        StringBuilder sb = new StringBuilder(scanner.nextLine());
+
+        int totalStrength = 0;
+        for (int i = 0; i < sb.length(); i++) {
+            char character = sb.charAt(i);
+
+            if (character == '>') {
+                int strength = Integer.parseInt(String.valueOf(sb.charAt(i + 1)));
+                totalStrength += strength;
+            } else if (character != '>' && totalStrength > 0) {
+                sb.deleteCharAt(i);
+                totalStrength--;
+                i--;
+            }
+        }
+
+        System.out.println(sb);
 
     }
 }
