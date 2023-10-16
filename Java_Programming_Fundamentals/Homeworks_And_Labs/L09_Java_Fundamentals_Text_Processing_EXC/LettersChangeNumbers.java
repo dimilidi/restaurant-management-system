f65374ab896bbe72be48dedb345ebd49ef1eaa3e
Lidiya <dimitrovalidia@yahoo.com>
2023-10-16 16:16:37 +0200

    .

--- Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/LettersChangeNumbers.java
+++ Java_Programming_Fundamentals/Homeworks_And_Labs/L09_Java_Fundamentals_Text_Processing_EXC/LettersChangeNumbers.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class LettersChangeNumbers {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
+//        before the number.
+//•	If it's uppercase, you divide the number by the letter's position in the alphabet.
+//•	If it's lowercase, you multiply the number with the letter's position in the alphabet.
+
+//        the letter after the number.
+//•	If it's uppercase, you subtract its position from the resulted number.
+//•	If it's lowercase, you add its position to the resulted number.
+
+
     }
 }
