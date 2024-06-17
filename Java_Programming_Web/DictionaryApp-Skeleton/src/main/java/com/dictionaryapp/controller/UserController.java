603be9f569bc29997294365c28d8e1b5def2d2a9
dimilidi <dimitrovalidia@yahoo.com>
2024-06-17 13:08:17 +0200

    show validation errors on registration

---
+++ Java_Programming_Web/DictionaryApp-Skeleton/src/main/java/com/dictionaryapp/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.dictionaryapp.controller;
+
+import com.dictionaryapp.model.dto.UserRegisterDTO;
+import com.dictionaryapp.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @ModelAttribute("registerData")
+    public UserRegisterDTO initRegisterData() {
+        return new UserRegisterDTO();
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerConfirm(@Valid UserRegisterDTO data,
+                                  BindingResult bindingResult,
+                                  RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("registerData", data);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.registerData", bindingResult);
+
+            return "redirect:/register";
+        }
+        return "redirect:/login";
+    }
+
+}
