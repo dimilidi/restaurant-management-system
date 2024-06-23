156ca9c40e7054f26b5ff734a9c98e71097df821
dimilidi <dimitrovalidia@yahoo.com>
2024-06-24 01:11:55 +0200

    refactored general structure

---
+++ Java_Programming_Web/Spring_Fundamentals/PaintingsCollectors/src/test/java/bg/softuni/paintingcollectors/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.paintingcollectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
