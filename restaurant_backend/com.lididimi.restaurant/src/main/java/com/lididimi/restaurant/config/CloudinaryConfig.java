61371512a877f8974fbfed8053eebb64e3053bd6
dimilidi <dimitrovalidia@yahoo.com>
2024-08-03 10:35:34 +0200

    BE: more tests added - 67% line coverage

--- restaurant_backend/com.lididimi.restaurant/src/main/java/com/lididimi/restaurant/config/CloudinaryConfig.java
+++ restaurant_backend/com.lididimi.restaurant/src/main/java/com/lididimi/restaurant/config/CloudinaryConfig.java
@@ -1,9 +1,11 @@
 package com.lididimi.restaurant.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,21 +13,32 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud-name}")
+
+    private final Environment env;
+/*
+   // @Value("${cloudinary.cloud-name}")
     private String cloudName;
 
-    @Value("${cloudinary.api-key}")
+    //@Value("${cloudinary.api-key}")
     private String apiKey;
 
-    @Value("${cloudinary.api-secret}")
-    private String apiSecret;
+    //@Value("${cloudinary.api-secret}")
+    private String apiSecret;*/
+
+    public CloudinaryConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", cloudName);
+   /*     config.put("cloud_name", cloudName);
         config.put("api_key", apiKey);
-        config.put("api_secret", apiSecret);
+        config.put("api_secret", apiSecret);*/
+
+        config.put("cloud_name", env.getProperty("cloudinary.cloud-name"));
+        config.put("api_key", env.getProperty("cloudinary.api-key"));
+        config.put("api_secret", env.getProperty("cloudinary.api-secret"));
         return new Cloudinary(config);
     }
 }
